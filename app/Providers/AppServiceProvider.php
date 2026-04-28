393a18940b4846b0ee543041ede2758492e22a4d
abuel3ees <abdurahmanluay@gmail.com>
2026-04-28 21:58:30 +0300

    DigitalOcean deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
-        if(app()->isProduction()) {
+        if (app()->isProduction()) {
             URL::forceScheme('https');
         }
     }
